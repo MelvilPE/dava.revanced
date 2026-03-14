c7db8c630f06f6ab2cf85b0ac82a4c3b981eff3d
MelvilPE <48417412+MelvilPE@users.noreply.github.com>
2026-03-14 12:16:52 +0100

    Update ParticleEffectComponentTest.cpp

--- Programs/UnitTests/DavaFramework/Sources/Tests/ParticleEffectComponentTest.cpp
+++ Programs/UnitTests/DavaFramework/Sources/Tests/ParticleEffectComponentTest.cpp
@@ -15,93 +15,212 @@ DAVA_TESTCLASS (ParticleEffectComponentTest)
     DAVA_TEST (TestDeserializeLegacy)
     {
         ParticleEffectComponent* PEC = new ParticleEffectComponent();
-        KeyedArchive* archive = new KeyedArchive();
+        KeyedArchive* archive = CreateNewEffectLegacyArchive();
         SerializationContext* serializationContext = new SerializationContext();
 
         Scene* scene = new Scene();
         serializationContext->SetScene(scene);
 
-        archive->SetBool("pe.nestedEmitters", false);
-        archive->SetUInt32("pe.version", 42);
-        archive->SetBool("pe.stopWhenEmpty", true);
-        archive->SetFloat("pe.effectDuration", 10.5f);
-        archive->SetUInt32("pe.repeatsCount", 3);
-        archive->SetBool("pe.clearOnRestart", true);
-        archive->SetFloat("pe.startFromTime", 1.2f);
-
-        KeyedArchive* emittersArch = new KeyedArchive();
-        KeyedArchive* emitter0 = new KeyedArchive();
-        emitter0->SetString("emitter.filename", "emitter.yaml");
-        emitter0->SetVector3("emitter.position", Vector3(1.0f, 1.0f, 1.0f));
-        emittersArch->SetArchive(KeyedArchive::GenKeyFromIndex(0), emitter0);
-        archive->SetArchive("pe.emitters", emittersArch);
-        archive->SetUInt32("pe.emittersCount", 1);
-
         PEC->Deserialize(archive, serializationContext);
+
+        TEST_VERIFY(PEC->IsNestedEmitters() == false);
+        TEST_VERIFY(PEC->IsUpdatedFromGame() == false);
 
         TEST_VERIFY(PEC->GetEmittersCount() == 1);
         TEST_VERIFY(PEC->GetEmitterInstance(0)->GetEmitter() != nullptr);
 
-        //SafeRelease(PEC);
+        String emitterFilename = PEC->GetOriginalConfigPath(0).GetStringValue();
+        TEST_VERIFY(HasYamlExtension(emitterFilename) == true);
+
         SafeRelease(archive);
-        //SafeRelease(serializationContext);
-        SafeRelease(emittersArch);
-        SafeRelease(emitter0);
         SafeRelease(scene);
     }
 
-    DAVA_TEST (TestDeserializeNestedSkip)
+    DAVA_TEST (TestDeserializeNested)
     {
         ParticleEffectComponent* PEC = new ParticleEffectComponent();
-        KeyedArchive* archive = new KeyedArchive();
+        KeyedArchive* archive = CreateNewEffectNestedArchive();
         SerializationContext* serializationContext = new SerializationContext();
 
         Scene* scene = new Scene();
         serializationContext->SetScene(scene);
 
-        archive->SetBool("pe.nestedEmitters", true);
-
         PEC->Deserialize(archive, serializationContext);
 
+        TEST_VERIFY(PEC->IsNestedEmitters() == true);
+        TEST_VERIFY(PEC->IsUpdatedFromGame() == false);
+
+        // We don't even try to load any emitters
         TEST_VERIFY(PEC->GetEmittersCount() == 0);
 
-        //SafeRelease(PEC);
         SafeRelease(archive);
-        //SafeRelease(serializationContext);
         SafeRelease(scene);
     }
 
     DAVA_TEST (TestDeserializeNestedParticlesGod)
     {
         ParticleEffectComponent* PEC = new ParticleEffectComponent();
-        KeyedArchive* archive = new KeyedArchive();
+        KeyedArchive* archive = CreateNewEffectNestedParticlesGodArchive();
         SerializationContext* serializationContext = new SerializationContext();
 
         Scene* scene = new Scene();
         serializationContext->SetScene(scene);
 
-        archive->SetBool("pe.nestedEmitters", true);
-        archive->SetBool("pe.updatedFromGame", true);
-
-        Vector<VariantType> emitters;
-        KeyedArchive* emitter0 = new KeyedArchive();
-        emitter0->SetString("emitter.filename", "emitter.yaml");
-        emitter0->SetVector3("emitter.position", Vector3(1.0f, 1.0f, 1.0f));
-        VariantType var;
-        var.SetKeyedArchive(emitter0);
-        emitters.push_back(var);
-        archive->SetVariantVector("pe.emitters", emitters);
-
         PEC->Deserialize(archive, serializationContext);
-
+        
+        TEST_VERIFY(PEC->IsNestedEmitters() == true);
         TEST_VERIFY(PEC->IsUpdatedFromGame() == true);
+
         TEST_VERIFY(PEC->GetEmittersCount() == 1);
         TEST_VERIFY(PEC->GetEmitterInstance(0)->GetEmitter() != nullptr);
 
-        //SafeRelease(PEC);
+        String emitterFilename = PEC->GetOriginalConfigPath(0).GetStringValue();
+        TEST_VERIFY(HasYamlExtension(emitterFilename) == true);
+
         SafeRelease(archive);
-        //SafeRelease(serializationContext);
-        SafeRelease(emitter0);
         SafeRelease(scene);
+    }
+
+    KeyedArchive* CreateNewEffectLegacyArchive()
+    {
+        String archiveMock = R"(
+keyedArchive:
+    comp.typename:
+        string: "ParticleEffectComponent"
+    pe.emitters:
+        keyedArchive:
+            0000:
+                keyedArchive:
+                    emitter.filename:
+                        string: "LeopardTest/welding.yaml"
+                    emitter.position:
+                        Vector3: [0.000000, 0.000000, 0.000000]
+    pe.clearOnRestart:
+        bool: true
+    pe.repeatsCount:
+        uint32: 4294967295
+    pe.effectDuration:
+        float: 10000.000000
+    pe.stopWhenEmpty:
+        bool: false
+    pe.emittersCount:
+        uint32: 1
+    pe.startFromTime:
+        float: 0.000000
+    pe.version:
+        uint32: 1
+    ro.flags:
+        uint32: 1
+)";
+        KeyedArchive* newArchive = new KeyedArchive();
+        newArchive->LoadFromYamlString(archiveMock);
+        return newArchive;
+    }
+
+        KeyedArchive* CreateNewEffectNestedArchive()
+    {
+        String archiveMock = R"(
+keyedArchive:
+    comp.typename:
+        string: "ParticleEffectComponent"
+    pe.emitters:
+        variantVector:
+        -   keyedArchive:
+                '##name':
+                    string: "<Unknown class>"
+                emitter.data:
+                    variantVector:
+                    -   keyedArchive:
+                            emitter.id:
+                                uint64: 282
+                emitter.position:
+                    Vector3: [0.000000, 0.000000, 0.000000]
+                emitter.rotation:
+                    Vector4: [0.000000, 0.000000, 0.000000, 1.000000]
+                emitter.scale:
+                    Vector3: [1.000000, 1.000000, 1.000000]
+    pe.clearOnRestart:
+        bool: true
+    pe.repeatsCount:
+        uint32: 4294967295
+    pe.effectDuration:
+        float: 10000.000000
+    pe.stopWhenEmpty:
+        bool: false
+    pe.emittersCount:
+        uint32: 1
+    pe.startFromTime:
+        float: 0.000000
+    pe.version:
+        uint32: 1
+    ro.flags:
+        uint32: 1
+    pe.nestedEmitters:
+        bool: true
+)";
+        KeyedArchive* newArchive = new KeyedArchive();
+        newArchive->LoadFromYamlString(archiveMock);
+        return newArchive;
+    }
+
+    KeyedArchive* CreateNewEffectNestedParticlesGodArchive()
+    {
+        String archiveMock = R"(
+keyedArchive:
+    comp.typename:
+        string: "ParticleEffectComponent"
+    pe.emitters:
+        variantVector:
+        -   keyedArchive:
+                '##name':
+                    string: "<Unknown class>"
+                emitter.data:
+                    variantVector:
+                    -   keyedArchive:
+                            emitter.id:
+                                uint64: 282
+                emitter.position:
+                    Vector3: [0.000000, 0.000000, 0.000000]
+                emitter.rotation:
+                    Vector4: [0.000000, 0.000000, 0.000000, 1.000000]
+                emitter.scale:
+                    Vector3: [1.000000, 1.000000, 1.000000]
+                emitter.filename:
+                    string: "LeopardTest/welding.yaml"
+    pe.clearOnRestart:
+        bool: true
+    pe.repeatsCount:
+        uint32: 4294967295
+    pe.updatedFromGame:
+        bool: true
+    pe.effectDuration:
+        float: 10000.000000
+    pe.stopWhenEmpty:
+        bool: false
+    pe.emittersCount:
+        uint32: 1
+    pe.startFromTime:
+        float: 0.000000
+    pe.version:
+        uint32: 1
+    ro.flags:
+        uint32: 1
+    pe.nestedEmitters:
+        bool: true
+)";
+        KeyedArchive* newArchive = new KeyedArchive();
+        newArchive->LoadFromYamlString(archiveMock);
+        return newArchive;
+    }
+
+    bool HasYamlExtension(String value)
+    {
+        bool isYamlFilename = false;
+        if (value.find(".yaml") != String::npos)
+        {
+            isYamlFilename = true;
+        }
+
+        return isYamlFilename;
     }
 };
